c78028c908951f5e91442e1a3d85d78b73bc4422
RicharDev <pinedaricardo587@gmail.com>
2024-06-01 15:15:30 -0600

    Estructura de carpetas lista.

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidebarMenuComponent } from './layout/sidebar-menu/sidebar-menu.component';
+
+
+
+@NgModule({
+  declarations: [
+    SidebarMenuComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DashboardModule { }
